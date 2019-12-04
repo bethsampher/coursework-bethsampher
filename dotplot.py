5cbe5809597cb0fece65335903ffd6712bd6c1ea
bethsampher <bs15@sanger.ac.uk>
2019-12-04 15:31:32 +0000

    Wrote tests for function to get sequence
    Tests fail:
    ================================================================================================================== FAILURES ==================================================================================================================
    __________________________________________________________________________________________________________ test_get_sequence_short ___________________________________________________________________________________________________________
    
        def test_get_sequence_short():
            lines = ['>ID', 'ATCG']
    >       assert get_sequence(lines) == 'ATCG'
    E       AssertionError: assert None == 'ATCG'
    E        +  where None = get_sequence(['>ID', 'ATCG'])
    
    test_dotplot.py:23: AssertionError
    ___________________________________________________________________________________________________________ test_get_sequence_long ___________________________________________________________________________________________________________
    
        def test_get_sequence_long():
            lines = ['>ID', 'ATCG', 'ATCGATCG', 'A']
    >       assert get_sequence(lines) == 'ATCGATCGATCGA'
    E       AssertionError: assert None == 'ATCGATCGATCGA'
    E        +  where None = get_sequence(['>ID', 'ATCG', 'ATCGATCG', 'A'])
    
    test_dotplot.py:27: AssertionError
    ====================================================================================================== 2 failed, 3 deselected in 0.09s =======================================================================================================
    
    I also added a docstring for read_lines

--- dotplot.py
+++ dotplot.py
@@ -3,7 +3,12 @@ TODO: write docstring
 """
 
 def read_lines(filename):
+    """ Returns list of lines read from given file (filename) """
     with open(filename) as file:
         contents = file.read()
         lines = contents.splitlines()
     return lines
+
+def get_sequence(lines):
+    """ Returns first sequence from list of file lines (lines), based on FASTA format """
+    pass
--- test_dotplot.py
+++ test_dotplot.py
@@ -17,3 +17,11 @@ with patch('dotplot.read_lines', side_effect=mocked_read_lines) as read_lines:
     def test_read_lines_with_multiple_lines():
         multiline_string = 'This is the first line\nThis is the second line\nThis is the third line'
         assert read_lines(multiline_string) == ['This is the first line', 'This is the second line', 'This is the third line']
+
+def test_get_sequence_short():
+    lines = ['>ID', 'ATCG']
+    assert get_sequence(lines) == 'ATCG'
+
+def test_get_sequence_long():
+    lines = ['>ID', 'ATCG', 'ATCGATCG', 'A']
+    assert get_sequence(lines) == 'ATCGATCGATCGA'
