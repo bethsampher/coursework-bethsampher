8333692537dc87d28ebaf9cbcd18a5ccabfa97fd
bethsampher <bs15@sanger.ac.uk>
2019-12-15 23:32:23 +0000

    Added docstrings and help messages

--- dotplot.py
+++ dotplot.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 """
-TODO: write docstring
+A script to read 2 FASTA files from the command line  and print a dotplot
+
+A sequence is retrieved from each file. The dotplot shows places where
+the sequences match, or where they complement each other if specified.
+There are other options to filter out lone matches/complements and to
+display the output with dots and slashes.
 """
 import argparse
 import sys
 from copy import deepcopy
 
 def get_lines_from_file(file_):
-    """ Returns list of lines read from given file (filename) """
+    """Returns list of lines read from given file"""
     contents = file_.read()
     lines = contents.splitlines()
     return lines
 
 def check_fasta_lines(lines):
-    """ TODO: write docstring """
+    """Returns true if given file lines are in FASTA format, false if not"""
     if not lines:
         return False
     for line in lines:
@@ -26,7 +31,10 @@ def check_fasta_lines(lines):
     return True
 
 def get_sequence_from_fasta_lines(lines):
-    """ Returns first sequence from list of file lines (lines), based on FASTA format """
+    """ Returns first sequence from given list of file lines
+
+    Based on FASTA format
+    """
     sequence = ''
     if check_fasta_lines(lines):
         for line in lines[1:]:
@@ -37,8 +45,11 @@ def get_sequence_from_fasta_lines(lines):
     return sequence
 
 def create_matches_table(seq_a, seq_b):
-    """ Returns a list of lists of matches
-    TODO: improve docstring
+    """Returns a list of lists of places where the sequences match
+
+    Each list is formed by comparing each item in seq_a with all the
+    items in seq_b. Matches appear in the list and non-matches
+    appear as whitespaces
     """
     table = []
     for base_a in seq_a:
@@ -52,7 +63,11 @@ def create_matches_table(seq_a, seq_b):
     return table
 
 def filter_matches(table):
-    """ TODO: write docstring """
+    """Returns a filtered list of lists of matches
+
+    Changes matches which do not form part of a
+    forwards diagonal line to lowercase
+    """
     for row_index, row in enumerate(table):
         for cell_index, cell in enumerate(row):
             if (row_index == 0) and (cell_index + 1 == len(row)):
@@ -69,18 +84,27 @@ def filter_matches(table):
                 row[cell_index] = cell.lower()
     return table
 
-def ascii_filter(table, slash):
-    """ TODO: write docstring """
-    for row in table:
+def ascii_filter(filtered_table, slash):
+    """Returns a list of lists of matches filtered with symbols
+
+    Changes uppercase letters in filtered matches list to a
+    forward or back slash depending on what is specified and
+    lowercase letters to the period character
+    """
+    for row in filtered_table:
         for cell_index, cell in enumerate(row):
             if cell.strip().isupper():
                 row[cell_index] = slash
             elif cell.strip().islower():
                 row[cell_index] = '.'
-    return table
+    return filtered_table
 
 def find_palindromes(table):
-    """ TODO: write docstring """
+    """Returns a list of lists of matches filtered to show palindromes
+
+    Changes matches which do not form part of a backwards diagonal
+    line to lowercase
+    """
     for row_index, row in enumerate(table):
         for cell_index, cell in enumerate(row):
             if (row_index == 0) and (cell_index == 0):
@@ -98,7 +122,12 @@ def find_palindromes(table):
     return table
 
 def create_complement_table(seq_a, seq_b):
-    """ TODO: write docstring """
+    """Returns a list of lists of places where the sequences complement each other
+
+    Each list is formed by comparing each item in seq_a with all the items in
+    seq_b. DNA/RNA complements appear in the list as 'X's and non-complements
+    appear as whitespaces
+    """
     table = []
     for base_a in seq_a:
         row = []
@@ -111,7 +140,12 @@ def create_complement_table(seq_a, seq_b):
     return table
 
 def merge_tables(table_1, table_2):
-    """ TODO: write docstring """
+    """Returns a merged filtered and palindrome list of lists
+
+    Allows both the forward and backward diagonal matches to
+    be in the same table so the filter and palindrome options
+    can be combined
+    """
     merged_table = []
     for row_1, row_2 in zip(table_1, table_2):
         merged_row = []
@@ -126,25 +160,39 @@ def merge_tables(table_1, table_2):
     return merged_table
 
 def parse_command_line_args():
-    """ TODO: write docstring """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('file_a', type=argparse.FileType('r'))
-    parser.add_argument('file_b', type=argparse.FileType('r'))
-    parser.add_argument('-c', '--complement', action='store_true')
-    parser.add_argument('-f', '--filter', action='store_true')
-    parser.add_argument('-a', '--ascii', action='store_true')
-    parser.add_argument('-p', '--palindrome', action='store_true')
+    """Parses options from the command line, returns args"""
+    description = ('Prints dotplot based on matches in sequences from 2 '
+            'FASTA files. Abnormal exit if files not in correct format')
+    parser = argparse.ArgumentParser(description=description)
+    help_msg = 'first FASTA file'
+    parser.add_argument('file_a', type=argparse.FileType('r'), help=help_msg)
+    help_msg = 'second FASTA file'
+    parser.add_argument('file_b', type=argparse.FileType('r'), help=help_msg)
+    help_msg = 'instead of matches, finds DNA/RNA complements in sequences'
+    parser.add_argument('-c', '--complement', action='store_true', help=help_msg)
+    help_msg = 'converts fowards lone matches to lowercase'
+    parser.add_argument('-f', '--filter', action='store_true', help=help_msg)
+    help_msg = ('converts lone matches to dots and joined matches to slashes, '
+    'must be used with --filter or --palindrome')
+    parser.add_argument('-a', '--ascii', action='store_true', help=help_msg)
+    help_msg = 'converts backwards lone matches to lowercase'
+    parser.add_argument('-p', '--palindrome', action='store_true', help=help_msg)
     return parser.parse_args()
 
 def print_dotplot(seq_a, seq_b, table):
-    """ TODO: write docstring """
+    """Prints a dotplot
+
+    Given the 2 sequences and the
+    corresponding list of lists of
+    matches
+    """
     print('  ' + seq_b)
     print(' +' + len(seq_b) * '-')
     for base_a, row in zip(seq_a, table):
         print(base_a + '|' + ''.join(row))
 
 def main():
-    """ TODO: write docstring """
+    """Function invoked when this is run as a script"""
     args = parse_command_line_args()
     seq_a = get_sequence_from_fasta_lines(get_lines_from_file(args.file_a))
     seq_b = get_sequence_from_fasta_lines(get_lines_from_file(args.file_b))
